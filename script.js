b6dce0ddc529bf0f1c25ae500e252ddc17ad2c0c
JuanZunino <jczunino3010@gmail.com>
2026-03-30 13:46:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const btnTop = document.getElementById("btn-top");
+const btnTop = document.querySelector('.scroll-top-btn');
 
-//Evento de "click" (lleva al usuario al comienzo de la página)
-btnTop.addEventListener("click", function() {
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+};
+
+
+btnTop.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
-        behavior: "smooth"
+        behavior: 'smooth'
     });
-    
-    console.log("El usuario volvió al inicio");
 });
